eb69e6f30871eb170153815aa0e8c500630b157e
Tentraby <98299414+Tentraby@users.noreply.github.com>
2023-08-02 10:36:37 -0500

    Update comentario.router.ts

--- src/routes/comentario.router.ts
+++ src/routes/comentario.router.ts
@@ -1,13 +1,25 @@
-import express from 'express';
-import commentModel from 'src/models/comentario.model';
+"use strict";
 
-const comentarioRouter = express.Router();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _express = _interopRequireDefault(require("express"));
+// Ajusta la ruta de importación para usar './comentario.model'
+var _comentario = _interopRequireDefault(require("./comentario.model"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const comentarioRouter = _express.default.Router();
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 comentarioRouter.post('/', async (req, res) => {
-  const { content } = req.body;
-  const data = await commentModel.createComment(content);
-  res.status(200).send({ results: data });
+  const {
+    content
+  } = req.body;
+  const data = await _comentario.default.createComment(content);
+  res.status(200).send({
+    results: data
+  });
 });
+var _default = comentarioRouter;
+exports.default = _default;
 
-export default comentarioRouter;
