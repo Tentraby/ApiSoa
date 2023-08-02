6244788aa9eb6103665e71038d3cc7f3dace4b95
Tentraby <98299414+Tentraby@users.noreply.github.com>
2023-08-02 14:36:39 -0500

    Update comentario.router.ts

--- src/routes/comentario.router.ts
+++ src/routes/comentario.router.ts
@@ -5,13 +5,10 @@ const comentarioRouter = express.Router();
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 comentarioRouter.post('/', async (req, res) => {
-  const {
-    content
-  } = req.body;
-  const data = await _comentario.default.createComment(content);
-  res.status(200).send({
-    results: data
-  });
+  const { content } = req.body;
+  const data = await commentModel.createComment(content);
+  res.status(200).send({ results: data });
 });
-var _default = comentarioRouter;
-exports.default = _default;
+
+export default comentarioRouter;
+
